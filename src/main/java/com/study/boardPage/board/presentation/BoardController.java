017276dc57a313007e52979b23f7b8adf3977514
jmlee119 <jmlee119@naver.com>
2025-02-06 17:50:05 +0900

    feat : get ID 요청 응답 테스트

--- src/main/java/com/study/boardPage/board/presentation/BoardController.java
+++ src/main/java/com/study/boardPage/board/presentation/BoardController.java
@@ -6,12 +6,11 @@ import com.study.boardPage.board.dto.req.BoardCreateDto;
 import com.study.boardPage.board.dto.resp.BoardAllDto;
 import com.study.boardPage.board.dto.resp.BoardReadDto;
 import com.study.boardPage.board.infrastructure.BoardRepository;
+import com.study.boardPage.global.response.dto.ApiResponse;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 @RestController // controller는 view 를 반환 restcontroller data 반환
@@ -27,33 +26,33 @@ public class BoardController {
         this.boardRepository = boardRepository;
         this.redisTemplate = redisTemplate;
     }
-//    @GetMapping("/board/{id}")
-//    public ResponseEntity<BoardReadDto> getBoardID(@PathVariable(name="id") int id) {
-//        BoardReadDto board = boardService.getBoardById(id);
-//        if (board != null) {
-//            return new ResponseEntity<>(board, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//    }
-
     @GetMapping("/board/{id}")
-    public Board getBoardId(@PathVariable(name="id") int id) {
-        String channelKey = CACHE_PREFIX + id;
-
-        //1. redis 에서 캐시된 데이터 조회
-        Board chahedBoard = redisTemplate.opsForValue().get(channelKey);
-        if (chahedBoard != null) {
-            System.out.println("return 중");
-            return chahedBoard;
+    public ResponseEntity<ApiResponse<BoardReadDto>> getBoardID(@PathVariable(name="id") int id) {
+        BoardReadDto board = boardService.getBoardById(id);
+        if (board != null) {
+            return ResponseEntity.ok(ApiResponse.success(board));
         }
-        // 2. 캐시된 데이터가 없으면 db에서 조회
-        Board board = boardRepository.findById(id).orElseThrow(() -> new RuntimeException("찾을 수 없음"));
-
-        // 3. 조회한 데이터를 redis 에 캐싱 (TTL : 10분)
-        redisTemplate.opsForValue().set(channelKey, board, 10, TimeUnit.MINUTES);
-        return board;
-
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.fail("게시글을 찾을 수 없습니다",404));
     }
+
+//    @GetMapping("/board/{id}")
+//    public Board getBoardId(@PathVariable(name="id") int id) {
+//        String channelKey = CACHE_PREFIX + id;
+//
+//        //1. redis 에서 캐시된 데이터 조회
+//        Board chahedBoard = redisTemplate.opsForValue().get(channelKey);
+//        if (chahedBoard != null) {
+//            System.out.println("return 중");
+//            return chahedBoard;
+//        }
+//        // 2. 캐시된 데이터가 없으면 db에서 조회
+//        Board board = boardRepository.findById(id).orElseThrow(() -> new RuntimeException("찾을 수 없음"));
+//
+//        // 3. 조회한 데이터를 redis 에 캐싱 (TTL : 10분)
+//        redisTemplate.opsForValue().set(channelKey, board, 10, TimeUnit.MINUTES);
+//        return board;
+//
+//    }
 
     @GetMapping("/board")
     public ResponseEntity<BoardAllDto> getBoardAll() {
