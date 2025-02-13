d8bd5f605dc06df5015aa567a9f11a00946e6709
jmlee119 <jmlee119@naver.com>
2025-02-13 13:24:33 +0900

    fix : redis 설정 삭제

--- src/main/java/com/study/boardPage/board/presentation/BoardController.java
+++ src/main/java/com/study/boardPage/board/presentation/BoardController.java
@@ -20,12 +20,10 @@ public class BoardController {
 
     private final BoardService boardService;
     private final BoardRepository boardRepository;
-    private static final String CACHE_PREFIX = "board:";
-    private RedisTemplate<String,Board> redisTemplate;
-    public BoardController(BoardService boardService, BoardRepository boardRepository, RedisTemplate<String,Board> redisTemplate) {
+
+    public BoardController(BoardService boardService, BoardRepository boardRepository) {
         this.boardService = boardService;
         this.boardRepository = boardRepository;
-        this.redisTemplate = redisTemplate;
     }
     @GetMapping("/board/{id}")
     public ResponseEntity<ApiResponse<BoardReadDto>> getBoardID(@PathVariable(name="id") int id) {
@@ -35,25 +33,6 @@ public class BoardController {
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.fail("게시글을 찾을 수 없습니다",404));
     }
-
-//    @GetMapping("/board/{id}")
-//    public Board getBoardId(@PathVariable(name="id") int id) {
-//        String channelKey = CACHE_PREFIX + id;
-//
-//        //1. redis 에서 캐시된 데이터 조회
-//        Board chahedBoard = redisTemplate.opsForValue().get(channelKey);
-//        if (chahedBoard != null) {
-//            System.out.println("return 중");
-//            return chahedBoard;
-//        }
-//        // 2. 캐시된 데이터가 없으면 db에서 조회
-//        Board board = boardRepository.findById(id).orElseThrow(() -> new RuntimeException("찾을 수 없음"));
-//
-//        // 3. 조회한 데이터를 redis 에 캐싱 (TTL : 10분)
-//        redisTemplate.opsForValue().set(channelKey, board, 10, TimeUnit.MINUTES);
-//        return board;
-//
-//    }
 
     @GetMapping("/board")
     public ResponseEntity<ApiResponse<BoardAllDto>> getBoardAll() {
