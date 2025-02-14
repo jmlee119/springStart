c12cece8bd228a76fa2721ab0f0998695121f503
jmlee119 <jmlee119@naver.com>
2025-02-14 10:25:56 +0900

    feat : 에러 메세지 추가

--- src/main/java/com/study/boardPage/global/response/ErrorCode.java
+++ src/main/java/com/study/boardPage/global/response/ErrorCode.java
@@ -17,8 +17,15 @@ public enum ErrorCode {
     BOARD_READALL_FAILED(HttpStatus.BAD_REQUEST,Boolean.FALSE, 3003, "글 조회 불가"),
     BOARD_UPDATE_FAILED(HttpStatus.BAD_REQUEST,Boolean.FALSE, 3004, "글 수정 실패"),
     BOARD_DELETE_FAILED(HttpStatus.BAD_REQUEST,Boolean.FALSE, 3005, "글 삭제 실패"),
-    BOARD_TYPE_UNDEFINED(HttpStatus.BAD_REQUEST,Boolean.FALSE, 3006, "게시판 유형을 확인");
-
+    BOARD_TYPE_UNDEFINED(HttpStatus.BAD_REQUEST,Boolean.FALSE, 3006, "게시판 유형을 확인"),
+    //9000 ~ 9999
+    //오류 종류 : 공통 에러
+    VALIDATION_FAIL_ERROR(HttpStatus.BAD_REQUEST, Boolean.FALSE, 9000, "(exception error 메세지에 따름)"),
+    NOT_SUPPORTED_METHOD(HttpStatus.METHOD_NOT_ALLOWED, Boolean.FALSE, 9001, "(exception error 메세지에 따름"),
+    NOT_FOUND_URL(HttpStatus.NOT_FOUND, Boolean.FALSE, 9002, "요청하신 URL 을 찾을 수 없습니다."),
+    INVALID_REQUEST_DATA(HttpStatus.BAD_REQUEST, Boolean.FALSE, 9003, "데이터 저장 실패, 재시도 혹은 관리자에게 문의해주세요."),
+    FAIL(HttpStatus.BAD_REQUEST, Boolean.FALSE, 9999, "요청 응답 실패, 관리자에게 문의해주세요."),
+            ;
     private final HttpStatus httpStatus;
     private final Boolean isSuccess;
     private final Integer code;
