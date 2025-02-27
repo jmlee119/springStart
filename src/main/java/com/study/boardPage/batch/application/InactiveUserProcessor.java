123085348b6f47db5b79b6ad1e8d2a743f927c0a
jmlee119 <jmlee119@naver.com>
2025-02-27 17:32:03 +0900

    feat : processor 생성

---
+++ src/main/java/com/study/boardPage/batch/application/InactiveUserProcessor.java
@@ -0,0 +1,15 @@
+package com.study.boardPage.batch.application;
+
+import com.study.boardPage.users.domain.Users;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InactiveUserProcessor implements ItemProcessor<Users,Users> {
+    @Override
+    public Users process(Users users) {
+        users.setStatus(4);
+        return users;
+    }
+
+}
